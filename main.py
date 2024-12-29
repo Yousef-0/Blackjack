0ff3c04dab6c106ae2401cec04e3a223aec2e9a1
Yousef-0 <98151111+Yousef-0@users.noreply.github.com>
2024-12-29 06:50:35 -0500

    Update main.py
    This is the final output of the team for the project. It is simple, but it works.

--- main.py
+++ main.py
@@ -1,76 +1,149 @@
-Algorithm:
-
 import random
-number_of_players = input("Pppl plz")
+#import tkinter
 
-Current cards = [{♤1: 1},{A♤: 11}] - 52 cards
-function(cards){shuffle} <=> choosing cards randomly
+class Player:
+    Players = []
+    count = 0
+    
+    cards = []
+    total = 0
+    busted = False
+    aces = 0
+    
+    def __init__(self, i, player_name):
+        self.name = player_name
+        self.cards = []
+        self.total = 0
+        self.stay = False
+        self.busted = False
+        self.split = False
+        self.id = i
+#        self.label = None
+        Player.count += 1
+        
+    def add_card(self, card):
+        self.cards.append(card)
+        self.total += card_values[card]
+#        self.label.config(text=self.name + ": " +str(self.cards))
+        
+    def split_cards(self):
+        if self.cards[0] == self.cards[-1]:
+            for i in range(len(Player.Players)):
+                if Player.Players[i].id == self.id and Player.Players[i].name == self.name:
+                    print("debug")
+                    Player.Players.insert(i+1,(Player(self.id,self.name + '2')))
+                    Player.Players[i+1].add_card(self.cards.pop())
+            self.total = self.total/2
+        else:
+            print("you can't split silly")
+    def qA(self):
+        if 'A' in self.cards:
+            self.cards.remove('A')
+            self.cards.append('A_1')
+            self.total -= 10
+        else:
+            self.busted = True
+            self.stay = True
+
+    def card_value(self): 
+        self.total = 0
+        for card in self.cards:
+            self.total += card_values[card]
+            
+    def ace_converter(self):
+        if 'A' in self.cards:
+            self.cards.remove('A')
+            self.cards.append('A_1')    
 
 
-for i in range(player_number):
-        player = create new Player
-        players.append(player)
+card_draws = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+              '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+              '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+              '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+deck = list(card_draws)
+random.shuffle(deck)
+                
+running_deck = {'2': 4, '3': 4, '4': 4, '5':4, '6': 4, '7': 4, '8': 4, '9': 4, '10': 4, 'J': 4, 'Q': 4, 'K': 4, 'A': 4} 
 
-dealer = create new Dealer
-
-Construction of dealer {
-
-Method.qAuto{Card count of A - 1 or 11}
-
-}
-
-
-Construction of
-Object = {
-.  name:
-• current cards = [] - the same dicts
-.  current count: 
-.  stay: bool
-• bust; false
-• has splitten: bool
+card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11, 'A_1': 1}        
 
 
-Method.add a card {cards[random.random(0,52)]}
-Method: split(two cards of same value and face; in a players this player is replaced by [new player, new player] - somehow replace)
-Method.bust(not this.bust)
-Method.stay (not this.stay)
-Method.qAuto{Compares the count and chooses 11 or 1 preferably 11 }
-}
+number_of_players = int(input("Pppl plz "))
 
-game = {"continue": bool }
+#window = thinter.Tk()
 
-initial_distribution:
-        for players in players:
-                 player.addcard
-                 player.addcard
-                 announce(player.card)
-       bet = input("deal with the devil")
-        dealer.addcard
-        dealer.addcard
-        announce(dealer.card[0])
- 
+for i in range(number_of_players):
+        Player.Players.append(Player(i, input("name plz ")))
+        Player.Players[i].add_card(deck.pop())
+        Player.Players[i].add_card(deck.pop())
+#        Player.Players[i].label = tkinter.label(window,text= Player.Players[i].name + ": " +str(Player.Players[i].cards))
+#        Player.Players[i].label.pack()        
+        
+Player.Players.append(Player(-1, "dealer"))
+dealer = Player.Players[-1]
+Player.Players[-1].add_card(deck.pop())
+print(dealer.cards)
+Player.Players[-1].add_card(deck.pop())
+#Player.Players[-1].label = tkinter.label(window,text= Player.Players[i].name + ": " +str(Player.Players[i].cards))
+#Player.Players[-1].label.pack()   
 
-while game[continue]:
+#frame = tkinter.Frame(window, borderwidth=4, relief=tkinter.GROOVE)
+#buttondraw = tkinter.Button(frame,text="draw",command=click)
+#buttonstay = tkinter.Button(frame,text="stay",command=click)
+#buttonsplit = tkinter.Button(frame,text="split",command=click)
+#buttondraw.pack()
+#buttonstay.pack()
+#buttonsplit.pack()
 
-if dealer.count > 21:
-         Print("you lost 300rub")
+if dealer.total == 21:
+    print(dealer.cards)
+    print("U lose")
 
-          for players in players:
-                  check whether there are players stayying
-                   ask the person about each method -> calls each function
-                   announce
+blackjack = False
+i = 0
+while i < len(Player.Players)-1:
+    player = Player.Players[i]
+    first_turn = True
+    while player.stay == False:
+        print(player.name + str(player.cards))
+        if first_turn == True:
+            action = input("Do you want to draw, split, or stay? ").strip().lower()
+        else:
+            action = input("Do you want to draw or stay? ").strip().lower()
+        if action == "stay":
+            player.stay = True
+        elif action == "draw":
+            player.add_card(deck.pop())
+            if player.total > 21:
+                player.qA()
+            elif player.total == 21:
+                player.stay = True
+                blackjack = True
+        elif action == "split" and first_turn == True:
+            player.split_cards()
+        first_turn = False
+    print(player.cards)
+    i += 1
+    
+while dealer.total < 17:
+    dealer.add_card(deck.pop())
+    if dealer.total > 21:
+        dealer.qA()
+    print(dealer.cards)
+if dealer.busted == True:
+    print("U win")
+elif dealer.total == 21:
+    print("U lose")
+flag = False
+for player in Player.Players:
+    if player.busted == False:
+        if player.total > dealer.total:
+            print(player.name + " you won")
+            flag = True
+if not flag and dealer.busted == False:
+    print("lose")
 
-                   if player.count >= 21:
-                             print("You won 3000 rub", )
-
-While dealer.count < 17:
-         dealer.addcard()
-
-if dealer.count > 21:
-         Print("you lost 300rub")
-
-for player in players:
-        if player.bust == False:
-                if player.count > dealer.count
-                          print("Ypu won 300rub")
-Print("you lost")
+print(dealer.cards)
+print(len(Player.Players))
+    
+#window.mainloop()
